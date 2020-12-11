cf0c41763fc37efab86aafa630ec7b6847703f05
root <root@mtribe.co.za>
2020-12-11 17:06:42 +0200

    inital commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.POSTGRES_DB,
+  pgUser: process.env.POSTGRES_USER,
+  pgPassword: process.env.POSTGRES_PASSWORD,
+  pgPort: process.env.PGPORT
+};
